de93c976b4de68442d21b69bd373fb3b8a490943
User <addisumm@gmail.com>
2022-03-03 22:18:00 -0600

    2-testing-sql-queries

---
+++ software-testing/software-testing-1-starting-point/src/main/java/com/amigoscode/testing/customer/CustomerRegistrationController.java
@@ -0,0 +1,17 @@
+package com.amigoscode.testing.customer;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("api/v1/customer-registration")
+public class CustomerRegistrationController {
+    @PutMapping
+    public void registerNewCustomer( @Valid @RequestBody  CustomerRegistrationRequest request ){
+
+    }
+}
